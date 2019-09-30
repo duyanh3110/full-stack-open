e674b3f3173fd879dc26eaea4eeca55b07239645
Nguyen Duy Anh <duyanh19962012@gmail.com>
2019-09-30 19:06:59 +0300

    1.6 & 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,62 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
+
+const Button = ({onClick, text}) => {
+    return(
+        <button onClick={onClick}>
+            {text}
+        </button>
+    )
+}
+
+const Result = ({name, value}) => {
+    return (
+        <p>{name} {value}</p>
+    )
+}
 
 const App = () => {
     // const-definitions
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
-    const Header = (props) => {
-        return (
-            <h1>{props.course}</h1>
-        )
-    }
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-    const Part = (props) => {
-        return (
-            <p>
-                {props.part} {props.exercise}
-            </p>
-        )
-    }
+    const sum = good + neutral + bad;
+    const ave = (good * 1 + neutral * 0 - bad * 1)/sum;
+    const pos = (good/sum) * 100;
 
-    const Content = (props) => {
-        return (
-            <div>
-                <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-                <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-                <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
-            </div>
-        )
-    }
-
-    const Total = (props) => {
-        return (
-            <p>
-                Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-            </p>
-        )
-    }
-
-    return (
+    return(
         <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+            <h1>give feedback</h1>
+            <Button onClick={() => setGood(good + 1)} text="good" />
+            <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+            <Button onClick={() => setBad(bad + 1)} text="bad" />
+            <h1>statistics</h1>
+            <Result name="good" value={good} />
+            <Result name="neutral" value={neutral} />
+            <Result name="bad" value={bad} />
+            <Result name="all" value={sum} />
+            <Result name="average" value={ave ? ave : 0} />
+            <Result name="possitive" value={pos ? pos + " %" : 0 + " %"} />
         </div>
     )
 }
