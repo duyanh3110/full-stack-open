a0ade656eede794379bd99ef9e9d6de5f09647a0
Nguyen Duy Anh <duyanh19962012@gmail.com>
2019-10-02 20:21:48 +0300

    1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,18 +1,38 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({onClick, text}) => {
-    return(
+const Button = ({ onClick, text }) => {
+    return (
         <button onClick={onClick}>
             {text}
         </button>
     )
 }
 
-const Result = ({name, value}) => {
+const Statistic = ({ text, value }) => {
     return (
-        <p>{name} {value}</p>
+        <p>{text} {value}</p>
     )
+}
+
+const Statistics = (props) => {
+    if (props.sum === 0) {
+        return (
+            <div>No feedback given</div>
+        )
+    }
+    else {
+        return (
+            <div>
+                <Statistic text="good" value={props.good} />
+                <Statistic text="neutral" value={props.neutral} />
+                <Statistic text="bad" value={props.bad} />
+                <Statistic text="all" value={props.sum} />
+                <Statistic text="average" value={props.ave} />
+                <Statistic text="possitive" value={props.pos} />
+            </div>
+        )
+    }
 }
 
 const App = () => {
@@ -22,22 +42,24 @@ const App = () => {
     const [bad, setBad] = useState(0);
 
     const sum = good + neutral + bad;
-    const ave = (good * 1 + neutral * 0 - bad * 1)/sum;
-    const pos = (good/sum) * 100;
+    const ave = (good * 1 + neutral * 0 - bad * 1) / sum;
+    const pos = (good / sum) * 100;
 
-    return(
+    return (
         <div>
             <h1>give feedback</h1>
             <Button onClick={() => setGood(good + 1)} text="good" />
             <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
             <Button onClick={() => setBad(bad + 1)} text="bad" />
             <h1>statistics</h1>
-            <Result name="good" value={good} />
-            <Result name="neutral" value={neutral} />
-            <Result name="bad" value={bad} />
-            <Result name="all" value={sum} />
-            <Result name="average" value={ave ? ave : 0} />
-            <Result name="possitive" value={pos ? pos + " %" : 0 + " %"} />
+            <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                sum={sum}
+                ave={ave ? ave : 0}
+                pos={pos ? pos + " %" : 0 + " %"}
+            />
         </div>
     )
 }
